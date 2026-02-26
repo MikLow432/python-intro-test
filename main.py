174a77096b2cc7eaafd3287923b6abb94e785f8c
MikLow432 <mik.low@gmx.de>
2026-02-26 12:29:26 +0100

    setup

--- main.py
+++ main.py
@@ -1,20 +1,20 @@
-import my_python_package as mpp
 import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
+import my_python_package as mpp
+
 
 def main():
     print("Hello from python-intro-test!")
-
-
 if __name__ == "__main__":
     main()
 
 
 print(mpp.add(1,2))
-
 print(mpp.add(10,100))
 
 
 
-fig = plt.figure()
-print(fig)
+plt.figure()
+plt.plot([1,2,3],[4,5,6])
+"""plt.show()"""
